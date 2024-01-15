ebaf966cf3454b3c5323c35bd8f9dfb4ab9aabc6
Shariful4447 <shariful.islam4447@gmail.com>
2024-01-15 19:41:22 +0100

    header section update

--- src/Pages/Shared/Header/Header.jsx
+++ src/Pages/Shared/Header/Header.jsx
@@ -1,9 +1,12 @@
-
+import logo from '../../../assets/logo.png';
+import moment from 'moment';
 
 const Header = () => {
     return (
-        <div>
-            <h2 className="text-3xl"> Header</h2>
+        <div className='text-center m-2'>
+            <img  src={logo} alt="" />
+            <p className='text-2xl'>Always Ride for truth, never Betray</p>
+            <p className='text-xl'>{moment().format('MMMM D, YYYY')}</p>
         </div>
     );
 };
