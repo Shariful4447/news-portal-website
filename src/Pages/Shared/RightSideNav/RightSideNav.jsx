d1ad80df0cab642d88057eedd966db589caf83dd
Shariful4447 <shariful.islam4447@gmail.com>
2024-01-16 09:26:10 +0100

    Login with gmail and github added

--- src/Pages/Shared/RightSideNav/RightSideNav.jsx
+++ src/Pages/Shared/RightSideNav/RightSideNav.jsx
@@ -1,9 +1,22 @@
-
+import { FaGoogle,FaGithubSquare } from 'react-icons/fa';
 
 const RightSideNav = () => {
     return (
         <div>
-            <h2 className='text-3xl'>Right side nav</h2>
+            <div className='p-4'>
+                <h2 className="text-3xl">Login Here</h2>
+            </div>
+            <button className="btn btn-outline w-full text-xl">
+                <FaGoogle></FaGoogle>
+                Google
+                
+            </button>
+            <button className="btn btn-outline w-full text-xl mt-2">
+                <FaGithubSquare></FaGithubSquare>
+                GIthub
+                
+            </button>
+
         </div>
     );
 };
