ade572d8485b9b49dbecfc7f74adc9de76376d92
Shariful4447 <shariful.islam4447@gmail.com>
2024-01-17 10:32:19 +0100

    update firebase.config

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,7 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-
+    apiKey: "AIzaSyBOrLoeMHvNO4GAEWoxLCXrr8xRi38fimQ",
+    authDomain: "news-portal-website-c4662.firebaseapp.com",
+    projectId: "news-portal-website-c4662",
+    storageBucket: "news-portal-website-c4662.appspot.com",
+    messagingSenderId: "867497887925",
+    appId: "1:867497887925:web:c0a19fd759589ab90216fc"
 };
 
 // Initialize Firebase
