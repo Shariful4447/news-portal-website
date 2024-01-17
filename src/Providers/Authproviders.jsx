a61bacfdae04fd60130f3336593452f4668a4df9
Shariful4447 <shariful.islam4447@gmail.com>
2024-01-17 23:23:00 +0100

    Page loading state maintained

--- src/Providers/Authproviders.jsx
+++ src/Providers/Authproviders.jsx
@@ -8,15 +8,19 @@ const auth = getAuth(app);
 
 const Authproviders = ({children}) => {
     const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
     const createUser = (email, password) => {
+        setLoading(true);
         return createUserWithEmailAndPassword(auth, email, password);
 
     };
 
     const signIn = (email, password) => {
+        setLoading(true);
         return signInWithEmailAndPassword(auth, email, password);
     }
    const logOut = () =>{
+    setLoading(true);
         return signOut(auth);
     }
 
@@ -24,6 +28,7 @@ const Authproviders = ({children}) => {
         const unSubscribe = onAuthStateChanged(auth, currentUser =>{
             console.log('user in the state changed', currentUser);
             setUser(currentUser);
+            setLoading(false);
         });
         return ()=>{
             unSubscribe();
@@ -31,6 +36,7 @@ const Authproviders = ({children}) => {
     },[])
     const authinfo = {
         user,
+        loading,
         createUser,
         signIn,
         logOut
