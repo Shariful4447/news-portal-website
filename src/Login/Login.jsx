5c2730be8964b8db07db3408ea243db335f02fc7
Shariful4447 <shariful.islam4447@gmail.com>
2024-01-17 09:40:06 +0100

    login page design done

--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -1,9 +1,38 @@
+import Navbar from "../Pages/Shared/Navbar/Navbar";
 
 
 const Login = () => {
     return (
         <div>
-            <h2>this is login</h2>
+            <Navbar></Navbar>
+                    
+
+                <div className="max-w-96 mx-auto text-center">
+                    <div>
+                        <h2>Please login</h2>
+                    </div>
+                    <form className="card-body">
+                        <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Email</span>
+                        </label>
+                        <input type="email" placeholder="email" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Password</span>
+                        </label>
+                        <input type="password" placeholder="password" className="input input-bordered" required />
+                        <label className="label">
+                            <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                        </label>
+                        </div>
+                        <div className="form-control mt-6">
+                        <button className="btn btn-primary">Login</button>
+                        </div>
+                    </form>   
+                </div>    
+
         </div>
     );
 };
